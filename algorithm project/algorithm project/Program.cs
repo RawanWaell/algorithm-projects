ddcc395f0588445c467a0d9e544113294d3a038a
RawanWaell <121647707+RawanWaell@users.noreply.github.com>
2024-12-17 10:54:14 -0800

    Add files via upload

---
+++ algorithm project/algorithm project/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void HeapSort(int[] array)
+    {
+        int n = array.Length;
+
+        // Build heap
+        for (int i = n / 2 - 1; i >= 0; i--)
+            Heapify(array, n, i);
+
+        // Extract elements from heap
+        for (int i = n - 1; i > 0; i--)
+        {
+            // Swap root with the last element
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // Heapify the reduced heap
+            Heapify(array, i, 0);
+        }
+    }
+
+    static void Heapify(int[] array, int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest])
+            largest = left;
+
+        if (right < n && array[right] > array[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            Heapify(array, n, largest);
+        }
+    }
+
+    static void Main()
+    {
+        int[] array = { 4, 10, 3, 5, 1 };
+
+        Console.WriteLine("Before sorting:");
+        Console.WriteLine(string.Join(" ", array));
+
+        HeapSort(array);
+
+        Console.WriteLine("\nAfter sorting:");
+        Console.WriteLine(string.Join(" ", array));
+    }
+}
---
+++ project algorithm 2/project algorithm 2/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+class Program
+{
+    class Edge
+    {
+        public int u, v, weight;
+        public Edge(int u, int v, int weight)
+        {
+            this.u = u;
+            this.v = v;
+            this.weight = weight;
+        }
+    }
+
+    static int[] parent;
+
+    static int Find(int x)
+    {
+        if (parent[x] != x)
+            parent[x] = Find(parent[x]);
+        return parent[x];
+    }
+
+    static void Union(int x, int y)
+    {
+        int rootX = Find(x);
+        int rootY = Find(y);
+        if (rootX != rootY)
+            parent[rootX] = rootY;
+    }
+
+    static void Kruskal(int n, Edge[] edges)
+    {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        var sortedEdges = edges.OrderBy(e => e.weight).ToArray();
+        int mstWeight = 0;
+
+        foreach (var edge in sortedEdges)
+        {
+            int u = edge.u, v = edge.v;
+            if (Find(u) != Find(v))
+            {
+                Union(u, v);
+                Console.WriteLine($"Edge: ({u}, {v}) with weight {edge.weight}");
+                mstWeight += edge.weight;
+            }
+        }
+
+        Console.WriteLine($"Total MST weight: {mstWeight}");
+    }
+
+    static void Main()
+    {
+        int n = 4;
+        Edge[] edges = new Edge[]
+        {
+            new Edge(0, 1, 10),
+            new Edge(0, 2, 6),
+            new Edge(0, 3, 5),
+            new Edge(1, 3, 15),
+            new Edge(2, 3, 4)
+        };
+
+        Kruskal(n, edges);
+    }
+}
